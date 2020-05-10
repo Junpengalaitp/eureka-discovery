e853f462a4d8be915dd601bdd31f0410edeb94be
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-10 16:33:11 +0800

    update config server url

--- src/main/java/com/alaitp/discovery/EurekaDiscoveryService.java
+++ src/main/java/com/alaitp/discovery/EurekaDiscoveryService.java
@@ -1,4 +1,4 @@
-package com.alaitp.discoveryservice;
+package com.alaitp.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +11,4 @@ public class EurekaDiscoveryService {
     public static void main(String[] args) {
         SpringApplication.run(EurekaDiscoveryService.class, args);
     }
-
 }
